671f2a4906c2303c8d99d454fe360ac922a5c614
Farisem <b022210095@student.utem.edu.my>
2023-11-21 00:57:47 +0800

    SUCESSFUL ADD

--- index.js
+++ index.js
@@ -2,9 +2,47 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 app.use(express.json())
-app.get('/', (req, res) => {
-res.send('Hello')
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://FARIS:Nyas1811@atlascluster.2hyuslj.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.post('/register', (req, res) => {
+
+  client.db("WEEK4").collection("users").insertOne({
+    "username": req.body.username,
+    "password": req.body.password
+  })
+  res.send('Hello World!')
 })
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  res.send('Login Succesful')
+})
+
 app.listen(port, () => {
-console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
